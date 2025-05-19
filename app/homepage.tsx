b354427af08418d31563f2310acc52bf9b42250f
Navneet2605 <navneetpcs3771@gmail.com>
2025-05-19 20:08:41 +0530

    Routes and Sidebar configuration

---
+++ app/homepage.tsx
@@ -0,0 +1,12 @@
+import SideBar from "./sidebar";
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <main>
+        <SideBar/>
+    </main>
+  )
+}
+
+export default Homepage
---
+++ app/sidebar.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+      Sidebar
+    </div>
+  )
+}
+
+export default SideBar
